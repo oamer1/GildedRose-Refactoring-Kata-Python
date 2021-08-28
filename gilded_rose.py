cdfb878154c0ec7bf12e9469dac83e023b991bbe
Omar Amer <oamer@windowslive.com>
2021-08-29 00:40:57 +0200

    Abstract Backstage item logic into a method

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,6 +5,16 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_backstage_passes_item(self, item):
+        """Handle the logic of "Backstage passes to a TAFKAL80ETC concert" """
+
+        if item.sell_in < 11 and item.quality < 50:
+            item.quality = item.quality + 2
+        if item.sell_in < 6 and item.quality < 50:
+            item.quality = item.quality + 1
+        if item.sell_in < 0:
+            item.quality = 0
+
     def update_quality(self):
         special_items = [
             "Aged Brie",
@@ -15,16 +25,13 @@ class GildedRose(object):
             if item.name not in special_items and item.quality > 0:
                 item.quality = item.quality - 1
 
-            if item.name in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert"):
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.update_backstage_passes_item(item)
+
+            if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -34,9 +41,6 @@ class GildedRose(object):
                 if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0
 
 
 class Item:
