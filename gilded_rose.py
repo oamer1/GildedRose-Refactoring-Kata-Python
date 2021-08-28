503c866c7aced639494ae3ec060f2261b6f9d4c0
Omar Amer <oamer@windowslive.com>
2021-08-29 00:52:27 +0200

    Abstract quality updating into a method

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,13 +5,19 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def increment_quality(self, quality, increment_unit):
+        """Handles quality increments and ensures quality >0 and <50"""
+        if quality > 0 and quality < 50:
+            quality = quality + increment_unit
+        return quality
+
     def update_backstage_passes_item(self, item):
         """Handle the logic of "Backstage passes to a TAFKAL80ETC concert" """
 
-        if item.sell_in < 11 and item.quality < 50:
-            item.quality = item.quality + 2
-        if item.sell_in < 6 and item.quality < 50:
-            item.quality = item.quality + 1
+        if item.sell_in < 11:
+            item.quality = self.increment_quality(item.quality, 2)
+        if item.sell_in < 6:
+            item.quality = self.increment_quality(item.quality, 1)
         if item.sell_in < 0:
             item.quality = 0
 
@@ -22,25 +28,23 @@ class GildedRose(object):
             "Sulfuras, Hand of Ragnaros",
         ]
         for item in self.items:
-            if item.name not in special_items and item.quality > 0:
+            if item.name not in special_items:
                 item.quality = item.quality - 1
 
             if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 self.update_backstage_passes_item(item)
 
             if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                item.quality = self.increment_quality(item.quality, 1)
 
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name not in special_items and item.quality > 0:
-                    item.quality = item.quality - 1
+                if item.name not in special_items:
+                    item.quality = self.increment_quality(item.quality, -1)
 
                 if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = self.increment_quality(item.quality, 1)
 
 
 class Item:
