6124a433ce875590f8c66c8a30a9ce32e2e3756e
Omar Amer <oamer@windowslive.com>
2021-08-28 23:43:53 +0200

    Refactor negative sell_in logic

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
+class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
+        special_items = [
+            "Aged Brie",
+            "Backstage passes to a TAFKAL80ETC concert",
+            "Backstage passes to a TAFKAL80ETC concert",
+        ]
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if (
+                item.name != "Aged Brie"
+                and item.name != "Backstage passes to a TAFKAL80ETC concert"
+            ):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
@@ -24,16 +32,15 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.name not in special_items and item.quality > 0:
+                    item.quality = item.quality - 1
+
+                if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality = item.quality + 1
+
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
 
 
 class Item:
