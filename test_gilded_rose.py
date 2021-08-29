22ade9c9842b851f0ba13fe3f02297597f5b0154
Omar Amer <oamer@windowslive.com>
2021-08-30 01:09:21 +0200

    Update tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import AgedBrie, Item, GildedRose
+
+
+# DRY easy to type constants
+AGED_BRIE = "Aged Brie"
+SULFURES = "Sulfuras, Hand of Ragnaros"
+BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+CONJURED = "Conjured"
 
 test_items = [
     Item("Bar", 10, 0),
-    Item("Aged Brie", 10, 0),
-    Item("Sulfuras, Hand of Ragnaros", 10, 0),
-    Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
-    Item("Conjured", 10, 0),
+    Item(AGED_BRIE, 10, 0),
+    Item(SULFURES, 10, 0),
+    Item(BACKSTAGE, 10, 0),
+    Item(CONJURED, 10, 0),
 ]
 # Exlude Sulfuras
-test_items_except_sulfuras = [
-    item for item in test_items if item.name != "Sulfuras, Hand of Ragnaros"
-]
+test_items_except_sulfuras = [item for item in test_items if item.name != SULFURES]
 
 
 class TestCommonTraits:
+    MAX_QUALITY = 50
     # The Quality of an item is never negative
     @pytest.mark.parametrize("item", test_items)
     def test_quality_never_negative(self, item):
@@ -32,46 +38,59 @@ class TestCommonTraits:
 
     # The Quality of an item is never more than 50
     @pytest.mark.parametrize("item", test_items)
-    def test_quality_never_greater_50(self, item):
-        item.quality = 50
+    def test_quality_never_greater_than_max_quality(self, item):
+        item.quality = self.MAX_QUALITY
         GildedRose([item]).update()
-        assert item.quality <= 50
+        assert item.quality <= self.MAX_QUALITY
 
 
 class TestNormalItems:
     """Test Normal items"""
 
+    normal_rate = -1
+
+    @pytest.fixture
+    def valid_item(self):
+        return Item("Foo", 10, 10)
+
+    @pytest.fixture
+    def passed_date_item(self):
+        return Item("Foo", 0, 10)
+
     # At the end of each day our system lowers both values for every item
-    def test_end_of_day_Normal_item(self):
+    def test_end_of_day(self, valid_item):
         """For Normal item lower sell_in and quality"""
-        item = Item("Foo", 10, 10)
+        item = valid_item
         initial_quality = item.quality
         GildedRose([item]).update()
         assert item.sell_in == 9
-        assert item.quality == initial_quality - 1
+        assert item.quality == initial_quality + self.normal_rate
 
     # Once the sell by date has passed, Quality degrades twice as fast
-    def test_quality_Normal_item_passed_date_degrades_twice(self):
-        item = Item("Foo", 0, 10)
+    def test_quality_item_passed_date_degrades_twice(self, passed_date_item):
+        item = passed_date_item
         initial_quality = item.quality
         GildedRose([item]).update()
-        assert item.quality == initial_quality - 2
+        assert item.quality == initial_quality + 2 * self.normal_rate
 
 
 class TestAgedBrie:
 
     # "Aged Brie" actually increases in Quality the older it gets
     def test_aged_brie_gets_older_quality_increase(self):
-        item = Item("Aged Brie", 10, 10)
+        item = Item(AGED_BRIE, 10, 10)
         initial_quality = item.quality
         GildedRose([item]).update()
-        assert item.quality > initial_quality
+        assert item.quality == initial_quality + 1
 
 
 class TestSulfuras:
     # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    # "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+    # This is a bit ambigous should the client code maintain the invarient quality=80 or GildedRose ?!
+    # Ask the goblin !!
     def test_Sulfuras_const_quality_and_sellin(self):
-        item = Item("Sulfuras, Hand of Ragnaros", 10, 10)
+        item = Item(SULFURES, 10, 80)
         initial_sell_in = item.sell_in
         initial_quality = item.quality
         GildedRose([item]).update()
@@ -86,7 +105,7 @@ class TestBackstagePasses:
 
     # "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
     def test_backstage_passes_gets_older_quality_increase(self):
-        item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)
+        item = Item(BACKSTAGE, 10, 10)
         initial_quality = item.quality
         GildedRose([item]).update()
         assert item.quality > initial_quality
@@ -94,7 +113,7 @@ class TestBackstagePasses:
     # Quality increases by 2 when there are 10 days or less
     @pytest.mark.parametrize("day", days_range_10_to_6_inc)
     def test_backstage_passes_10_days_or_less_increase_quality_by_2(self, day):
-        item = Item("Backstage passes to a TAFKAL80ETC concert", day, 10)
+        item = Item(BACKSTAGE, day, 10)
         initial_quality = item.quality
         GildedRose([item]).update()
         assert item.quality == initial_quality + 2
@@ -102,7 +121,7 @@ class TestBackstagePasses:
     # Quality increase by 3 when there are 5 days or less but
     @pytest.mark.parametrize("day", days_range_5_to_0_inc)
     def test_backstage_passes_5_days_or_less_increase_quality_by_3(self, day):
-        item = Item("Backstage passes to a TAFKAL80ETC concert", day, 5)
+        item = Item(BACKSTAGE, day, 5)
         initial_quality = item.quality
         GildedRose([item]).update()
         assert item.quality == initial_quality + 3
@@ -110,26 +129,20 @@ class TestBackstagePasses:
     # Quality drops to 0 after the concert
     @pytest.mark.parametrize("day", days_sample_less_than_0)
     def test_backstage_concert_finished_quality_0(self, day):
-        item = Item("Backstage passes to a TAFKAL80ETC concert", day, 5)
+        item = Item(BACKSTAGE, day, 5)
         GildedRose([item]).update()
         assert item.quality == 0
 
 
-class TestConjured:
+class TestConjured(TestNormalItems):
     """Test Conjured items"""
 
-    # At the end of each day our system lowers both values for every item
-    def test_end_of_day_Normal_item(self):
-        """For Normal item lower sell_in and quality"""
-        item = Item("Conjured", 10, 10)
-        initial_quality = item.quality
-        GildedRose([item]).update()
-        assert item.sell_in == 9
-        assert item.quality == initial_quality - 2
+    normal_rate = 2 * TestNormalItems.normal_rate
 
-    # Once the sell by date has passed, Quality degrades twice as fast as Normal
-    def test_quality_Conjured_item_passed_date_degrades_twice_as_Normal(self):
-        item = Item("Conjured", 0, 10)
-        initial_quality = item.quality
-        GildedRose([item]).update()
-        assert item.quality == initial_quality - 4
+    @pytest.fixture
+    def valid_item(self):
+        return Item(CONJURED, 10, 10)
+
+    @pytest.fixture
+    def passed_date_item(self):
+        return Item(CONJURED, 0, 10)
